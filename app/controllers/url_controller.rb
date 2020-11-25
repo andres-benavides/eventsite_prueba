8578bac9543f1e07698c142245e8e46625c5fad2
Andres <andre0190@gmail.com>
2020-11-25 16:46:05 -0500

    Validar usuario para poder guardar la URL

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -2,12 +2,15 @@ class UrlController < ApplicationController
   require 'net/http'
   include UrlHelper
 
+  before_action :auth_user
 
   #crear la url corta
   def short
     alexa_rank = alexa_rank params[:url]
-    domain = get_domain params[:url]
-    @url = Url.create url: params[:url], alexa_rank: alexa_rank, domain: domain
+    host = get_domain params[:url]
+    host = host.split "."
+    domain = host.count >= 3 ? host[host.count - 2] :  host[host.count - 1]
+    @url = @user.url.create url: params[:url], alexa_rank: alexa_rank, domain: domain
     @url.save!
     render json: {url_short: @url.url_short}
   end
@@ -26,6 +29,22 @@ class UrlController < ApplicationController
 
   def get_by_domain
     render json: Url.where("domain LIKE :dom", {:dom => "%#{params[:domain]}%"})
+  end
+
+  private
+  def auth_user
+    t_regx = /Bearer (\w+)/
+    headers = request.headers
+
+    if headers['Authorization'].present? && headers['Authorization'].match(t_regx)
+      token =  headers['Authorization'].match(t_regx)[1]
+      @user = User.find_by_auth_token token
+    elsif request.method == "GET"
+      false
+    else
+      render json:{error:"unathotization"}, status: :unauthorized
+    end
+
   end
 
 end
